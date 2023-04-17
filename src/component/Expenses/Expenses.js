233fe5a062f9859059f0d9688fbcde537548162a
Lilii <lilii-ann.older@voco.ee>
2023-04-17 13:19:34 +0300

    React projekt - näidisprojekt kulude arvestamiseks

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,12 +1,20 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 
 
 function Expenses(props) {
+
+    const ExpenseFilterHandler = (selectedYear) => {
+        console.log(selectedYear);
+    }
+
+
     return (
         <Card className="expenses">
+            <ExpensesFilter onExpenseFilter={ExpenseFilterHandler}></ExpensesFilter>
             <ExpenseItem expenseData={props.expenses[0]}></ExpenseItem>
             <ExpenseItem expenseData={props.expenses[1]}></ExpenseItem>
             ))}
